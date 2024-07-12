6043e85e959c3da062617f3ea5be180597acfd73
minamarin <120408542+minamarin@users.noreply.github.com>
2024-07-11 23:08:42 -0400

    state management

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Main from './Pages/Main.jsx'
 
 const App = () => {
   return (
     <div className="home">
       <h1>hello</h1>
+      < Main />
     </div>
   );
 };
