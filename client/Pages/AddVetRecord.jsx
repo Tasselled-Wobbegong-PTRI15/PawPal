2dd952dd886cf7a3d4a27f4935e9509db1977bff
Justin Tran <justintran@Justins-MacBook-Pro-2.local>
2024-07-20 11:47:46 -0700

    finished vet records frontend

--- client/Pages/AddVetRecord.jsx
+++ client/Pages/AddVetRecord.jsx
@@ -2,40 +2,38 @@ import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import Navigation from "../Navigation.jsx";
 
-const AddJournal = () => {
+const AddVetRecord = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const pet_id = queryParams.get('pet_id');
 
-  const [journalInfo, setJournalInfo] = useState({
-    title: '',
-    text_input: '',
-    photo_url: '',
+  const [vetRecordInfo, setVetRecordInfo] = useState({
+    date_of_visit: '',
+    description: '',
+    notes: '',
   });
 
   const handleChange = (e) => {
-    console.log('e.target.name ', e.target.name);
-    console.log('e.target.value ', e.target.value);
-    setJournalInfo({ ...journalInfo, [e.target.name]: e.target.value});
+    setVetRecordInfo({ ...vetRecordInfo, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     
     try {
-      const response = await fetch(`/api/journal?pet_id=${pet_id}`, {
+      const response = await fetch(`/api/vet_records?pet_id=${pet_id}`, {
         method: "POST",
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(journalInfo),
+        body: JSON.stringify(vetRecordInfo),
       });
 
       if (response.ok) {
-        navigate(`/journal?pet_id=${pet_id}`);
+        navigate(`/vetrecords?pet_id=${pet_id}`);
       } else {
-        console.log("Failed to add journal entry");
+        console.log("Failed to add vet record");
       }
     } catch (error) {
       console.log("Error happened in POST request", error);
@@ -45,42 +43,42 @@ const AddJournal = () => {
   return (
     <div>
       <Navigation pet_id={pet_id} />
-      <h2>Add Journal Entry</h2>
+      <h2>Add Vet Record</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="title">Title:</label>
+          <label htmlFor="date_of_visit">Date of Visit:</label>
           <input
-            type="text"
-            id="title"
-            name="title"
-            value={journalInfo.title}
+            type="date"
+            id="date_of_visit"
+            name="date_of_visit"
+            value={vetRecordInfo.date_of_visit}
             onChange={handleChange}
             required
           />
         </div>
         <div>
-          <label htmlFor="textInput">Text:</label>
+          <label htmlFor="description">Description:</label>
           <textarea
-            id="textInput"
-            name="text_input"
-            value={journalInfo.text_input}
+            id="description"
+            name="description"
+            value={vetRecordInfo.description}
             onChange={handleChange}
             required
           />
         </div>
         <div>
-          <label htmlFor="photo">Photo:</label>
-          <input
-            id="photo"
-            name="photo_url"
-            value={journalInfo.photo_url}
+          <label htmlFor="notes">Notes:</label>
+          <textarea
+            id="notes"
+            name="notes"
+            value={vetRecordInfo.notes}
             onChange={handleChange}
           />
         </div>
-        <button type="submit">Add Journal Entry</button>
+        <button type="submit">Add Vet Record</button>
       </form>
     </div>
   );
 };
 
-export default AddJournal;
+export default AddVetRecord;
--- client/Pages/VetRecords.jsx
+++ client/Pages/VetRecords.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes, useLocation, Link } from "react-router-dom";
 import Navigation from "../Navigation.jsx";
 import Header from "../components/Header.jsx"
 
@@ -10,19 +10,38 @@ const VetRecords = () => {
   const pet_id = queryParams.get('pet_id');
   const [vetEntries, setVetEntries] = useState([]);
 
-  // useEffect(() => {
-  //   const fetchVetEntries = async () => {
-  //     try {
-  //       // const response = await fetch(`api/vetrecords?`)
-  //     }
-  //   }
-  // })
+  useEffect(() => {
+    const fetchVetEntries = async () => {
+      try {
+        const response = await fetch(`api/all_records?pet_id=${pet_id}`);
+        console.log('fetch success')
+        const result = await response.json();
+        setVetEntries(result);
+      } catch (error) {
+        console.log("Error fetching vet entries", error);
+      }
+    };
+
+    fetchVetEntries();
+  }), [pet_id];
+
   return (
     <div>
       <Header />
       <Navigation pet_id={pet_id} />
       <h2>Vet Records</h2>
-
+      <Link to={`/addvetrecord?pet_id=${pet_id}`}>
+        <button>New Vet Record</button>
+      </Link>
+      <ul>
+        {vetEntries.map((entry) => (
+          <li key={entry.id}>
+            <p>Date of Visit: {new Date(entry.date).toLocaleDateString()}</p>
+            <p>Description: {entry.description}</p>
+            <p>Notes: {entry.notes}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
