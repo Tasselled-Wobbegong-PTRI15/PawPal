a994d599c7abdff0cbc4423fb2d96edd20c181ec
Justin Tran <justintran@Justins-MacBook-Pro-2.local>
2024-07-20 10:38:50 -0700

    changed vaccine to vet

--- client/Pages/VetRecords.jsx
+++ client/Pages/VetRecords.jsx
@@ -3,20 +3,28 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import Navigation from "../Navigation.jsx";
 import Header from "../components/Header.jsx"
 
-const VaccineRecords = () => {
+const VetRecords = () => {
   // access query parameter 
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const pet_id = queryParams.get('pet_id');
+  const [vetEntries, setVetEntries] = useState([]);
 
+  // useEffect(() => {
+  //   const fetchVetEntries = async () => {
+  //     try {
+  //       // const response = await fetch(`api/vetrecords?`)
+  //     }
+  //   }
+  // })
   return (
     <div>
       <Header />
       <Navigation pet_id={pet_id} />
-      <h2>Vaccine Records</h2>
+      <h2>Vet Records</h2>
 
     </div>
   );
 };
 
-export default VaccineRecords;
+export default VetRecords;
